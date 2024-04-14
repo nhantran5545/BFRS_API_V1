bca37bf2dafd0bde2012017b3b98c40d9d7d2f87
Tran Hoang Nguyen <nguyendk29dtm@gmail.com>
2024-04-14 22:10:11 +0700

    Add Test

---
+++ BFRS_BankTests/ApiWebApplicationFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BFRS_BankTests
+{
+    internal class ApiWebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration(config => { });
+            builder.ConfigureTestServices(services => { });
+        }
+    }
+}
